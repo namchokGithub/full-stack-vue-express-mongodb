1daa36d14cb959505cf93a12fafad036c897c5f7
Namchok <pech4751@gmail.com>
2020-06-24 15:47:11 +0700

    First commit | error

---
+++ serve/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParse.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server start on port ${port}, BearSoft Inc.`))
